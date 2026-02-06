4b8ad0e0d689547bf767660169d428aa679e898d
CrazyValky <ryanteokh@gmail.com>
2026-02-06 11:23:57 +0800

    Update IMovementStrategy.java

--- lwjgl3/src/main/java/io/github/Project/engine/interfaces/IMovementStrategy.java
+++ lwjgl3/src/main/java/io/github/Project/engine/interfaces/IMovementStrategy.java
@@ -1,14 +1,7 @@
 package io.github.Project.engine.interfaces;
 
-/**
- * Interface for movement strategies.
- * Allows different movement behaviors to be implemented.
- */
+import io.github.Project.engine.entities.Entity;
+
 public interface IMovementStrategy {
-    /**
-     * Updates the entity's idle state.
-     * @param vx Current velocity in x direction
-     * @param vy Current velocity in y direction
-     */
-    void updateIdleState(float vx, float vy);
+    void updateMovement(Entity entity, float deltaTime);
 }

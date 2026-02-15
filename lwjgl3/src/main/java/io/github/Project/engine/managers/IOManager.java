cd59521294a57a537955f0d04adc7c455d7a4630
corn0404 <2502445@sit.singaporetech.edu.sg>
2026-02-15 21:00:00 +0800

    updated io manager to not include keyboard, mouse and touch input

--- lwjgl3/src/main/java/io/github/Project/engine/managers/IOManager.java
+++ lwjgl3/src/main/java/io/github/Project/engine/managers/IOManager.java
@@ -1,121 +1,48 @@
 package io.github.Project.engine.managers;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.files.FileHandle;
 
 /**
  * Manages input/output operations.
- * Handles keyboard, mouse, and touch input.
+ * Handles file reading and writing.
  */
-public class IOManager implements InputProcessor {
-    private boolean[] keys;
-    private int mouseX;
-    private int mouseY;
-    private boolean mousePressed;
-    
+public class IOManager {
+
     /**
      * Creates a new IOManager.
      */
     public IOManager() {
-        keys = new boolean[512];
-        Gdx.input.setInputProcessor(this);
+        // No input processor registration (SRP-compliant)
     }
-    
+
     /**
-     * Checks if a key is currently pressed.
-     * @param keycode The key code to check
-     * @return true if pressed, false otherwise
+     * Reads a file from the assets directory.
+     * @param filePath The file path relative to assets
+     * @return File contents as String
      */
-    public boolean isKeyPressed(int keycode) {
-        if (keycode >= 0 && keycode < keys.length) {
-            return keys[keycode];
-        }
-        return false;
+    public String readInternalFile(String filePath) {
+        FileHandle file = Gdx.files.internal(filePath);
+        return file.readString();
     }
-    
+
     /**
-     * Gets the mouse X position.
-     * @return Mouse X coordinate
+     * Writes data to a local file.
+     * @param filePath The file path relative to local storage
+     * @param content The content to write
      */
-    public int getMouseX() {
-        return mouseX;
+    public void writeLocalFile(String filePath, String content) {
+        FileHandle file = Gdx.files.local(filePath);
+        file.writeString(content, false);
     }
-    
+
     /**
-     * Gets the mouse Y position.
-     * @return Mouse Y coordinate
+     * Checks if a file exists in internal storage.
+     * @param filePath The file path
+     * @return true if exists, false otherwise
      */
-    public int getMouseY() {
-        return mouseY;
-    }
-    
-    /**
-     * Checks if the mouse button is pressed.
-     * @return true if pressed, false otherwise
-     */
-    public boolean isMousePressed() {
-        return mousePressed;
-    }
-    
-    @Override
-    public boolean keyDown(int keycode) {
-        if (keycode >= 0 && keycode < keys.length) {
-            keys[keycode] = true;
-        }
-        return true;
-    }
-    
-    @Override
-    public boolean keyUp(int keycode) {
-        if (keycode >= 0 && keycode < keys.length) {
-            keys[keycode] = false;
-        }
-        return true;
-    }
-    
-    @Override
-    public boolean keyTyped(char character) {
-        return false;
-    }
-    
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        mouseX = screenX;
-        mouseY = screenY;
-        mousePressed = true;
-        return true;
-    }
-    
-    @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        mouseX = screenX;
-        mouseY = screenY;
-        mousePressed = false;
-        return true;
-    }
-    
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
-        mouseX = screenX;
-        mouseY = screenY;
-        return true;
-    }
-    
-    @Override
-    public boolean touchCancelled(int screenX, int screenY, int pointer, int button) {
-        mousePressed = false;
-        return true;
-    }
-    
-    @Override
-    public boolean mouseMoved(int screenX, int screenY) {
-        mouseX = screenX;
-        mouseY = screenY;
-        return true;
-    }
-    
-    @Override
-    public boolean scrolled(float amountX, float amountY) {
-        return false;
+    public boolean fileExists(String filePath) {
+        return Gdx.files.internal(filePath).exists();
     }
 }
+
